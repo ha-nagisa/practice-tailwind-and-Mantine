66bae089ea7434eab4a1e6c37b3e4fdffedd4d88
ha-nagisa <nagisa18222@icloud.com>
2022-08-15 18:46:30 +0900

    環境構築

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+
+/** トップページ */
+const TopPage: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <h1>HELLO!</h1>
+      </main>
+    </div>
+  );
+};
+
+export default TopPage;
